7240d2bbe4f91395d694cbd05ad348fa19d25256
zeelrupapara <zeelrupapara@gmail.com>
2022-10-07 14:41:09 +0530

    feat: add button lib for vue

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from "vue";
+import Vue2Button from "@/vue2-button.vue";
+
+export default Vue.extend({
+  name: "ServeDev",
+  components: {
+    Vue2Button,
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue2-button>click</vue2-button>
+  </div>
+</template>
---
+++ src/vue2-button.vue
@@ -0,0 +1,18 @@
+<script>
+export default /*#__PURE__*/ {
+  name: "Vue2Button", // vue component name
+};
+</script>
+
+<template>
+  <button class="vue2-button"><slot /></button>
+</template>
+
+<style scoped>
+.vue2-button {
+  color: white;
+  font-weight: 500;
+  font: 1.5em sans-serif;
+  background-color: royalblue;
+}
+</style>
